3ffb313ee9d20676ce4723613033ae56cf0881f7
CarlosCedeira <cedeira05@gmail.com>
2025-03-04 12:56:24 +0100

    footer y optimizacion de css invalidos o no se usaban

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const screenWidth = window.innerWidth;
 
+  document.getElementById("airbnb").addEventListener("click", function () {
+    window.open(
+      "https://www.airbnb.es/rooms/36137898?source_impression_id=p3_1655367898_/hQK/RuyNsyx6LYw",
+      "_blank"
+    );
+  });
+
   if (screenWidth <= 768) {
     // Cargar script para dispositivos móviles
     loadScript("lightboxMobile.js");
